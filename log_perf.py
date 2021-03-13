769061617c7f02782afbff77a6ba979aa467bfea
Weixing Sun <weixing.sun@gmail.com>
2021-03-13 11:25:44 +0900

    format

--- log_perf.py
+++ log_perf.py
@@ -15,17 +15,16 @@ def lat_max(arr):
 def lat_99(arr):
     return int(numpy.percentile(arr, 99))
 
-def stats(csv):
+def mean_count_stats(csv):
     count = csv.groupby(INDEX).agg(['mean','count'])
     count.index.name = INDEX
     count.reset_index(inplace=True)
     count.columns = ["_".join(x) for x in count.columns.ravel()]
-    count.to_csv("count_"+args.type+".csv")
     count.rename(columns={ count.columns[0]: INDEX }, inplace = True)
     # df2 = df.rename(columns={'a': 'X', 'b': 'Y'})
     return count
 
-def latency(csv):
+def throughput_latency_stats(csv):
     tp_grp = csv.groupby(INDEX).agg({'batches':numpy.sum})
     lat_min_grp = csv.groupby(INDEX).agg({'delay':lat_min, 'proc':lat_min})
     lat_min_new = lat_min_grp.rename(columns={'delay': 'delay_min', 'proc': 'proc_min'})  # inplace=True
@@ -46,14 +45,13 @@ def latency(csv):
     lat_all = lat_all.reindex(columns=column_names)
     return lat_all
 
-def plot(df,t):
+def show_img(df,data_type,chart_type):
     fig, ax = plt.subplots(figsize=(20, 10))
     ax.set_xlabel('time (s)')
-    type_name = 'throughput'
-    title = args.type+" "+type_name+" (batches/sec)"
+    title = data_type+" "+chart_type+" (batches/sec)"
     mean_position = int(len(df[INDEX])/-25)
     y_mean = 0
-    if t == 't':
+    if chart_type == 'throughput':
         ax1 = df.plot(ax=ax, kind='line', x=INDEX, y='batches', label='batches', c='tab:blue')
         # print(list(df[INDEX]))
         y_min = df['batches'].min()
@@ -64,9 +62,8 @@ def plot(df,t):
         if args.detail == 'yes':
             plt.ylim([y_min,y_mean*3])
         plt.legend(loc='best')
-    elif t == 'l':
-        type_name = 'latency'
-        title = args.type+" "+type_name+" (ms)"
+    elif chart_type == 'latency':
+        title = data_type+" "+chart_type+" (ms)"
         #ax2 = df.plot(ax=ax, kind='line', x=INDEX, y='delay_min', label='delay_min', c='tab:green')
         #ax3 = df.plot(ax=ax, kind='line', x=INDEX, y='delay_99', label='delay_99', c='tab:gray')
         #ax4 = df.plot(ax=ax, kind='line', x=INDEX, y='delay_max', label='delay_max',c='tab:red')
@@ -80,9 +77,8 @@ def plot(df,t):
         if args.detail == 'yes':
             plt.ylim([0,y_mean*3])
         plt.legend(loc='best')
-    elif t == 's':
-        title = args.type+" "+type_name+" (mean/count)"
-        type_name = 'stats'
+    elif chart_type == 'batching_stats':
+        title = data_type+" "+chart_type+" (mean/count)"
         color1 = 'tab:red'
         color2 = 'tab:green'
         count_line = df.plot(ax=ax, kind='line', x=INDEX, y='batches_count', label='commit_times', c=color1)
@@ -108,7 +104,7 @@ def plot(df,t):
     plt.axis('on')
     plt.xticks(numpy.arange(df.shape[0]))
     if args.png == 'yes':
-        plt.savefig(args.type+'_'+type_name+'.png')
+        plt.savefig(data_type+'_'+type_name+'.png')
     else:
         plt.show()
 
@@ -129,34 +125,35 @@ def csv_trim(csv_file):
     cmd = ["sed -i 's/ms//g' "+csv_file]
     exec_cmd(cmd)
 
-def gen_csv(log_file):
-    csv_file = args.type+".csv"
-    csv_header(csv_file)
-    cmd = ["/bin/grep "+args.type+" "+log_file+" | /bin/grep delay| /bin/grep -v ERROR | /bin/awk -v OFS=',' '{print substr($2,1,8),$(NF-3), substr($(NF-1),7), substr($NF,6)}' >> "+csv_file]
+def gen_csv(log_file,log_type):
+    csv_file_name = log_type+".csv"
+    csv_header(csv_file_name)
+    cmd = ["/bin/grep "+log_type+" "+log_file+" | /bin/grep delay| /bin/grep -v ERROR | /bin/awk -v OFS=',' '{print substr($2,1,8),$(NF-3), substr($(NF-1),7), substr($NF,6)}' >> "+csv_file_name]
     exec_cmd(cmd)
-    csv_trim(csv_file)
+    csv_trim(csv_file_name)
+    return csv_file_name
 
 def skip_head_tail(csv):
     return csv[1:]
 
-def show_img():
-    gen_csv(args.log)
-    csv = load_csv(args.type+".csv")
-    # csv = skip_head_tail(csv)
-    count = stats(csv)
-    # print(count)
-    # print(args.csv+" latency (ms) and throughput (batches/sec)")
-    lat = latency(csv)
-    # print(lat.mean())
-    lat.to_csv("sum_"+args.type+".csv")
-    plot(lat,'t')
-    plot(lat,'l')
-    plot(count,'s')
+def proc_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--log", help="spade log file", type=str, required=True)
+    parser.add_argument("-t", "--type", help="perf data type", type=str, default='ord')
+    parser.add_argument("-i", "--png", help="save as png",   type=str, default='yes')
+    parser.add_argument("-s", "--skip", help="skip head/tail",   type=str, default='yes')
+    parser.add_argument("-d", "--detail", help="zoom into details",   type=str, default='yes')
+    return parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--log", help="spade log file", type=str, required=True)
-parser.add_argument("-t", "--type", help="perf data type", type=str, default='ord')
-parser.add_argument("-i", "--png", help="save as png",   type=str, default='yes')
-parser.add_argument("-s", "--skip", help="skip head/tail",   type=str, default='yes')
-parser.add_argument("-d", "--detail", help="zoom into details",   type=str, default='yes')
-args = parser.parse_args()
+args = proc_args()
+filename = gen_csv(args.log,args.type)
+csv = load_csv(filename)
+# csv = skip_head_tail(csv)
+lat = throughput_latency_stats(csv)
+lat.to_csv("sum_"+filename)          # save csv
+show_img(lat,args.type,'throughput')
+show_img(lat,args.type,'latency')
+
+count = mean_count_stats(csv)
+count.to_csv("count_"+filename)
+show_img(count,args.type,'batching_stats')
